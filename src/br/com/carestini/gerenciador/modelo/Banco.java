a866aad3d50e96b8fffaa31140a0f3eec848e708
Wagner Carestini <wcarestini@gmail.com>
2019-06-09 14:35:18 -0300

    Criado um formulario de login

--- src/br/com/carestini/gerenciador/modelo/Banco.java
+++ src/br/com/carestini/gerenciador/modelo/Banco.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Banco {
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList();
 	private static Integer chaveSequencial = 1;
 	
 	static {
@@ -17,6 +18,17 @@ public class Banco {
 		empresa2.setNome("DBC Company");
 		Banco.lista.add(empresa);
 		Banco.lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("wagner");
+		u1.setSenha("asd");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("lucas");
+		u2.setSenha("1234");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa e){
@@ -45,6 +57,15 @@ public class Banco {
 		for(Empresa empresa : lista){
 			if(empresa.getId() == id){
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : listaUsuarios){
+			if(usuario.ehIgual(login, senha)){
+				return usuario;
 			}
 		}
 		return null;
